9f860efdc0af229d8fa32703cc2915620c9fd9c8
Matheus Bento <matheus.th.bento@gmail.com>
2023-12-04 17:46:36 -0300

    Fixing generation of round matches to prevent the same team having more than 1 match per round

--- src/LeagueTable/Service/RoundRobinLeagueService/RoundRobinLeagueService.cs
+++ src/LeagueTable/Service/RoundRobinLeagueService/RoundRobinLeagueService.cs
@@ -45,53 +45,104 @@ namespace LeagueTable.Service.RoundRobinLeagueService
 
             foreach (string team in teams)
             {
-                IList<string> remainingTeams = new List<string>(teams);
-
-                remainingTeams.Remove(team);
-
                 // 1st half
+                //
+                // For the 1st half of the table each team gets their table
+                // generated round by round with a random opponent picked for
+                // each round
                 for (int i = 0; i < roundCount / 2; i++)
                 {
                     if (table.ElementAtOrDefault(i) == null)
                     {
                         table.Add(new List<RoundRobinLeagueMatch>());
                     }
-
-                    RoundRobinLeagueMatch match;
-
-                    bool homeMatch = this.GetRandomNumber() % 2 == 0;
-                    string opponent = this.GetRandomTeam(remainingTeams);
-
-                    if (homeMatch)
+                    // If the current team already has a match in this round
+                    // we skip to the next one
+                    else if (table[i].Any(r => r.HomeTeam == team ||
+                                               r.AwayTeam == team))
                     {
-                        match = new RoundRobinLeagueMatch
-                        {
-                            HomeTeam = team,
-                            AwayTeam = opponent
-                        };
-                    }
-                    else
-                    {
-                        match = new RoundRobinLeagueMatch
-                        {
-                            HomeTeam = opponent,
-                            AwayTeam = team
-                        };
+                        continue;
                     }
 
-                    table[i].Add(match);
-                    remainingTeams.Remove(opponent);
+                    var remainingTeams = new List<string>(teams);
+                    remainingTeams.Remove(team);
+
+                    string opponent = null;
+
+                    // If a match against the chosen opponent already has a
+                    // match in the current round we remove the opponent from
+                    // the pool of remaining teams until we find a team that
+                    // does not have a match on this round yet
+                    do
+                    {
+                        if (remainingTeams.Count() == 0)
+                        {
+                            opponent = null;
+                            continue;
+                        }
+
+                        opponent = this.GetRandomTeam(remainingTeams);
+                        remainingTeams.Remove(opponent);
+                    }
+                    while (
+                        table[i].Any(r => r.HomeTeam == opponent ||
+                                          r.AwayTeam == opponent)
+                    );
+
+                    if (opponent != null)
+                    {
+                        RoundRobinLeagueMatch match;
+                        bool homeMatch = this.GetRandomNumber() % 2 == 0;
+
+                        if (homeMatch)
+                        {
+                            match = new RoundRobinLeagueMatch
+                            {
+                                HomeTeam = team,
+                                AwayTeam = opponent
+                            };
+                        }
+                        else
+                        {
+                            match = new RoundRobinLeagueMatch
+                            {
+                                HomeTeam = opponent,
+                                AwayTeam = team
+                            };
+                        }
+
+                        table[i].Add(match);
+                    }
                 }
 
                 // 2nd half
+                //
+                // For the 2nd half of the table the home and away teams are
+                // simply flipped compared to the 1st half matches
                 for (int i = roundCount / 2; i < roundCount; i++)
                 {
+                    int firstHalfRoundIdx = i - (roundCount / 2);
+
                     if (table.ElementAtOrDefault(i) == null)
                     {
                         table.Add(new List<RoundRobinLeagueMatch>());
                     }
-
-                    int firstHalfRoundIdx = i - (roundCount / 2);
+                    // If the current team already has a match in this round
+                    // we skip to the next one
+                    else if (table[i].Any(r => r.HomeTeam == team ||
+                                               r.AwayTeam == team))
+                    {
+                        continue;
+                    }
+                    // If the current team doesn't have a match generated for
+                    // the corresponding 1st half round we skip to the next one
+                    else if (
+                        !table[firstHalfRoundIdx]
+                            .Any(r => r.HomeTeam == team ||
+                                      r.AwayTeam == team))
+                    {
+                        continue;
+                    }
 
                     RoundRobinLeagueMatch firstHalfMatch =
                         table[firstHalfRoundIdx]
